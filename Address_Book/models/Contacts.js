533560dbd1d95c234f9addd5f43b69d00b13d37c
Bipin87 <sahubipin484@gmail.com>
2025-03-07 16:04:06 +0530

    [Bipin Kumar Sahu] Add: Validate the address book Contacts attributes

--- Address_Book/models/Contacts.js
+++ Address_Book/models/Contacts.js
@@ -1,8 +1,17 @@
-// UC1: Ability to create address book Contacts
+// UC2: Validate Contects Attributes
 
 // Class contact to ensure the following details from the user
-class Contacts{
-    constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){
+class Contacts {
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.validateName(firstName);
+        this.validateName(lastName);
+        this.validateAddress(address, "Address", 4);
+        this.validateMinLength(city, "City", 4);
+        this.validateMinLength(state, "State", 4);
+        this.validateZip(zip);
+        this.validatePhone(phoneNumber);
+        this.validateEmail(email);
+        
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -12,5 +21,41 @@ class Contacts{
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
+
+
+    validateName = (name) => {
+        const regexPattern = /^[A-Z][a-zA-Z]{2,}$/
+        if (!regexPattern.test(name)) {
+            throw new Error(`${name} is not valid please check try again!`);
+        }
+    }
+
+    validateAddress = (value, minLength) => {
+        if (value.length < minLength) {
+            throw new Error(`${value} for address is not valid it must have atleast ${minLength} characters`);
+        }
+    }
+
+    validateZip = (zipCode) => {
+        const zipPattern = /^[0-9]{5,6}$/;
+        if(!zipPattern.test){
+            throw new Error("Zip must be of length 5 or 6 and only contains number " + zipCode);
+        }
+    }
+
+    validatePhone = (phone)=> {
+        const phonePattern = /^[0-9]{10}$/; 
+        if (!phonePattern.test(phone)) {
+            throw new Error("Phone number must be a valid 10-digit number.");
+        }
+    }
+
+    validateEmail = (email)=> {
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailPattern.test(email)) {
+            throw new Error("Invalid email format.");
+        }
+    }
 }
-MediaSourceHandle.exports = Contacts;
+
+export default Contacts;
